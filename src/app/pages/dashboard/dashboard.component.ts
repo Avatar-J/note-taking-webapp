5284a14c2d4caf9846dc09fd10e5ba42976f6710
Jummai <jummyhassan5@gmail.com>
2025-06-30 11:49:08 +0000

    add note card

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { Note } from '../../models/note';
+import { NoteCardComponent } from '../../components/note-card/note-card.component';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [NoteCardComponent, RouterLink],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  dataService = inject(DataService);
+  notes!: Note[];
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.dataService.notes$.subscribe((data) => (this.notes = data));
+  }
+
+  onCreateNewNote() {
+    this.router.navigate(['/create']);
+  }
 }
