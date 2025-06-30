118dea011ff7c4204a5483e0561f5189524471e8
Jummai <jummyhassan5@gmail.com>
2025-06-30 10:45:39 +0000

    add pages and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { CreateNoteComponent } from './pages/create-note/create-note.component';
+import { ViewNoteComponent } from './pages/view-note/view-note.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ArchivedNoteComponent } from './pages/archived-note/archived-note.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'create',
+    component: CreateNoteComponent,
+  },
+  {
+    path: 'archived',
+    component: ArchivedNoteComponent,
+  },
+  {
+    path: 'notes/:id',
+    component: ViewNoteComponent,
+  },
+];
